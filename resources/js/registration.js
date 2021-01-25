77e9479b0c1e248005edf63d88c28347099be438
Jose De La Cruz <jose.delcruz0490@icloud.com>
2021-01-25 00:32:18 -0500

    Had form event listener look at all inputs for checking validity

--- resources/js/registration.js
+++ resources/js/registration.js
@@ -7,6 +7,7 @@ const password2 = document.getElementById('password-confirm');
 console.log(form);
 
 // Function
+// Show error outline
 function showError(input, message) {
   const formControl = input.parentElement;
   formControl.className = 'form-control error';
@@ -14,9 +15,16 @@ function showError(input, message) {
   small.innerText = message;
 }
 
+// Show Success Outline
 function showSuccess(input) {
   const formControl = input.parentElement;
   formControl.className = 'form-control success';
+}
+
+// Check if email is valid
+function isValidEmail(email) {
+  const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
 }
 
 // Event Listeners
@@ -28,5 +36,25 @@ form.addEventListener('submit', function (e) {
     showError(username, 'Username is required');
   } else {
     showSuccess(username);
+  }
+
+  if (email.value === '') {
+    showError(email, 'Email is required');
+  } else if(!isValidEmail(email.value)) {
+    showError(email, 'Email is not valid');
+  } else {
+    showSuccess(email);
+  }
+
+  if (password.value === '') {
+    showError(password, 'Password is required');
+  } else {
+    showSuccess(password);
+  }
+
+  if (password2.value === '') {
+    showError(password2, 'Password2 is required');
+  } else {
+    showSuccess(password2);
   }
 });
